33d6cfae4486d3e5179209f8fa8acf7a2ceea525
recca383 <sirpatrick121402@gmail.com>
2024-12-21 22:10:02 +0800

    Added BackendTester project for testing the class library without UI

---
+++ BackendTester/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using data = NimbusClassLibrary;
+
+namespace BackendTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            data.Data.DbConnect dbConnect = new data.Data.DbConnect();
+            Console.WriteLine("Connected");
+
+            Console.ReadLine(); 
+        }
+    }
+}

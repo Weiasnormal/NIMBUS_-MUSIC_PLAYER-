46979aa3137763103cff36f21c1716dbfde1b716
recca383 <sirpatrick121402@gmail.com>
2024-12-21 22:12:04 +0800

    Added Class Library project for pure backend that is used by both UI and backend tester

---
+++ NimbusClassLibrary/Data/DbConnect.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Data
+{
+    public class DbConnect
+    {
+        MySqlConnection conn = new MySqlConnection();
+        public DbConnect()
+        {
+            string connString =
+                "Data Source = 153.92.15.21;" +
+                " Initial Catalog = u936666569_Nimbus;" +
+                " Persist Security Info = True;" +
+                " User ID = u936666569_Nimbus;" +
+                " Password = Haduken@123456;";
+            try
+            {
+                conn.ConnectionString = connString;
+                 
+                conn.Open();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}

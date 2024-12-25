96cb1d0c921d72ebed3a9fa9589c7b43982f6f9f
recca383 <sirpatrick121402@gmail.com>
2024-12-25 18:01:57 +0800

    - Added Data Context to store temporary data for faster data fetching - Added GetAll** methods to fill the temporary data in Data Context - Switch from DbSet<T> to List<T>

---
+++ NimbusClassLibrary/Data/DBContext.cs
@@ -0,0 +1,23 @@
+﻿using NimbusClassLibrary.Model;
+using NimbusClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Data
+{
+    public class DBContext
+    {
+        public static ICollection<Song> songs = new List<Song>();
+        public static ICollection<Artist> artists = new List<Artist>();
+        public static ICollection<Playlist> playlists = new List<Playlist>();
+        public static ICollection<PlaylistSongs> playlistSongs = new List<PlaylistSongs>();
+
+
+
+    }
+}
--- NimbusClassLibrary/Data/DbConnect.cs
+++ NimbusClassLibrary/Data/DbConnect.cs
@@ -1,6 +1,11 @@
 ﻿using MySql.Data.MySqlClient;
+using NimbusClassLibrary.Model;
+using NimbusClassLibrary.Models;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.Entity;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
@@ -10,8 +15,10 @@ namespace NimbusClassLibrary.Data
 {
     public class DbConnect
     {
-        MySqlConnection conn = new MySqlConnection();
-        public DbConnect()
+
+        static MySqlConnection conn = new MySqlConnection();
+        #region Singleton Instance
+        private DbConnect()
         {
             string connString =
                 "Data Source = 153.92.15.21;" +
@@ -22,13 +29,153 @@ namespace NimbusClassLibrary.Data
             try
             {
                 conn.ConnectionString = connString;
-                 
+
                 conn.Open();
+
+
+
+                GetAllArtists();
+                GetAllPlaylists();
+                GetAllSongs();
+                GetAllPlaylistSongs();
+                
             }
             catch (Exception ex)
             {
                 throw new Exception(ex.Message);
             }
         }
+        private static DbConnect instance = null;
+        public static DbConnect Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new DbConnect();
+                }
+                return instance;
+            }
+        }
+        #endregion
+        #region Connection Options
+        public MySqlConnection GetConnection()
+        {
+            return conn;
+        }
+        public MySqlConnection RefreshConnection()
+        {
+            conn.Close();
+            instance = new DbConnect();
+
+            return conn;
+        }
+        #endregion
+
+        #region Get all tables
+        private static void GetAllSongs()
+        {
+            
+            MySqlCommand mySqlCommand = new MySqlCommand
+                (
+                    "SELECT * " +
+                    "FROM Songs"
+                , conn);
+
+            DbDataReader reader = mySqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Song song = new Song()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Title = reader["Title"].ToString(),
+                    IsFavorite = reader["IsFavorite"].Equals("1"),
+                    File_Path = reader["File_Path"].ToString(),
+                    Artist = DBContext.artists.FirstOrDefault
+                    (i => i.Id == Convert.ToInt32(reader["Artist_id"]))
+                };
+
+                DBContext.songs.Add(song);
+            }
+            reader.Close();
+        }
+        private static void GetAllArtists()
+        {
+
+            MySqlCommand mySqlCommand = new MySqlCommand
+                (
+                    "SELECT * " +
+                    "FROM Artists"
+                , conn);
+
+            DbDataReader reader = mySqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Artist artist = new Artist()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Display_Name = reader["Display_Name"].ToString(),
+                    IsFollowed = reader["IsFollowed"].Equals ("1"),
+                    IsFavorite = reader["IsFavorite"].Equals("1"),
+                    Profile_Pic = Encoding.ASCII.GetBytes(reader["Profile_Pic"].ToString())
+                };
+
+                DBContext.artists.Add(artist);
+            }
+            reader.Close();
+        }
+        
+        private static void GetAllPlaylists()
+        {
+
+            MySqlCommand mySqlCommand = new MySqlCommand
+                (
+                    "SELECT * " +
+                    "FROM Playlists"
+                , conn);
+
+            DbDataReader reader = mySqlCommand.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Playlist playlist = new Playlist()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Name = reader["Name"].ToString(),
+                    Thumbnail = Encoding.ASCII.GetBytes(reader["Thumbnail"].ToString())
+                };
+
+                DBContext.playlists.Add(playlist);
+            }
+            reader.Close();
+        }
+        private static async void GetAllPlaylistSongs()
+        {
+
+            MySqlCommand mySqlCommand = new MySqlCommand
+                (
+                    "SELECT * " +
+                    "FROM Playlist_Songs"
+                , conn);
+
+            DbDataReader reader = await mySqlCommand.ExecuteReaderAsync();
+
+            while (reader.Read())
+            {
+                PlaylistSongs playlistsongs = new PlaylistSongs()
+                {
+                    Id = Convert.ToInt32(reader["Id"]),
+                    Playlist = DBContext.playlists.FirstOrDefault(
+                        i => i.Id == Convert.ToInt32(reader["Playlist_id"])),
+                    Song = DBContext.songs.FirstOrDefault(
+                        i => i.Id == Convert.ToInt32(reader["Song_id"]))
+                };
+
+                DBContext.playlistSongs.Add(playlistsongs);
+            }
+        }
+        #endregion
     }
 }

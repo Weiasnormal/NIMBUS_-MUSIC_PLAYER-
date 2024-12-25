43a9e527128447057666583ee52826e1e89b3ba3
recca383 <sirpatrick121402@gmail.com>
2024-12-25 15:45:11 +0800

    Added Models

---
+++ NimbusClassLibrary/Models/Artist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Models
+{
+    public class Artist
+    {
+        public int Id { get; set; }
+        public string Display_Name { get; set; }
+        public bool IsFollowed { get; set; }
+        public bool IsFavorite { get; set; }
+        public byte[] Profile_Pic { get; set; }
+    }
+}
---
+++ NimbusClassLibrary/Models/Playlist.cs
@@ -0,0 +1,17 @@
+﻿using NimbusClassLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Models
+{
+    public class Playlist
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public byte[] Thumbnail { get; set; }
+        public ICollection<Song> Songs{ get; set; }
+    }
+}

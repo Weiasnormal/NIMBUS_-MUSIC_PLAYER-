432e3c7d7fc72af472cefffb884e3c9ecc71c5f4
recca383 <sirpatrick121402@gmail.com>
2024-12-25 17:59:46 +0800

    Model are now complete based on the ER Diagram

---
+++ NimbusClassLibrary/Model/PlaylistSongs.cs
@@ -0,0 +1,16 @@
+﻿using NimbusClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Model
+{
+    public class PlaylistSongs
+    {
+        public int Id { get; set; }
+        public Playlist Playlist { get; set; }
+        public Song Song { get; set; }
+    }
+}
---
+++ NimbusClassLibrary/Model/Song.cs
@@ -0,0 +1,27 @@
+﻿using NimbusClassLibrary.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Model
+{
+    public class Song
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsFavorite { get; set; }
+        public string File_Path { get; set; }
+        public Artist Artist { get; set; }
+
+        public override string ToString()
+        {
+            return $"ID          : {Id} " +
+                $"\n Title       : {Title}" +
+                $"\n Is Favorite : {IsFavorite}" +
+                $"\n File Path   : {File_Path}" +
+                $"\n Artist Name : {Artist.Display_Name}";
+        }
+    }
+}

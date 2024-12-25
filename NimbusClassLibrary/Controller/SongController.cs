619f4f4cf1ecc97ee911fc0b90abc392021a1e71
recca383 <sirpatrick121402@gmail.com>
2024-12-25 18:02:27 +0800

    First Controller Established for Songs

---
+++ NimbusClassLibrary/Controller/SongController.cs
@@ -0,0 +1,27 @@
+﻿using MySql.Data.MySqlClient;
+using NimbusClassLibrary.Data;
+using NimbusClassLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Controller
+{
+    public class SongController
+    {
+        MySqlConnection conn;
+        
+        public SongController()
+        {
+            DbConnect db = DbConnect.Instance;
+            conn = db.GetConnection();
+        }
+        public IEnumerable<Song> GetAllSongs()
+        {
+            return DBContext.songs.ToList();
+        }
+    }
+}

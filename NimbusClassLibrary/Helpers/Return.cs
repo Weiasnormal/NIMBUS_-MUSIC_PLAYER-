f83a63a390dce2093352c3500d122fe6e9e78fd1
recca383 <sirpatrick121402@gmail.com>
2024-12-27 00:15:21 +0800

    Added return for same interface as web API

---
+++ NimbusClassLibrary/Helpers/Return.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Helpers
+{
+    internal static class Return
+    {
+        public static bool OK(int execrows)
+        {
+            if (execrows > 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}

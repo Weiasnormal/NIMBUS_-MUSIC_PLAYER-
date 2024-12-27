57d4a342a7f128b40bb83af5e6a3f3dd8009773f
recca383 <sirpatrick121402@gmail.com>
2024-12-28 07:24:13 +0800

    Initialized interfaces

---
+++ NimbusClassLibrary/Interfaces/IController.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Interfaces
+{
+    internal interface IController<t> where t : IModel
+    {
+        T GetSingle<T>(int id) where T : t;
+        IEnumerable<T> GetCollection<T>() where T : t;
+        bool Create<T>(T t) where T : t;
+        bool Update<T>(T t) where T : t;
+        bool Delete<T>(T t) where T : t;
+    }
+}

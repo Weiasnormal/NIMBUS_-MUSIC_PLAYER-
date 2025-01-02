0d3100c18135d26da70830535bdccc17becf1d3e
recca383 <sirpatrick121402@gmail.com>
2025-01-02 17:56:14 +0800

    Created Global Libraries for Global use of data structures

---
+++ NimbusClassLibrary/Helpers/GlobalLibraries.cs
@@ -0,0 +1,19 @@
+﻿using NimbusClassLibrary.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NimbusClassLibrary.Helpers
+{
+    public static class GlobalLibraries
+    {
+        // Current queue of songs to be played (LinkedList)
+        public static LinkedList<Song> Playing_Song = new LinkedList<Song>();
+
+        // Stack to keep track of previously played songs
+        public static Stack<Song> Previous_Songs = new Stack<Song>();
+
+    }
+}
